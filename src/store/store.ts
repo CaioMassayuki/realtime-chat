74125fa705a9f8d66548219aa423081614f85bad
Caio <caiomassayuki.y@gmail.com>
2024-02-13 21:45:47 -0300

    Adjust some test utils, remove unused mock and add devtools ans persist to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,29 +1,12 @@
-// import { StateCreator, create } from "zustand";
-// import createLiveChatSessionSlice, { LiveChatSessionSlice } from "./slices/livechatSessionSlice";
-// import createChatHistorySlice, { ChatHistorySlice } from "./slices/chathistorySlice";
-// import createUsersSlice, { UsersSlice } from "./slices/usersSlice";
-
-// export interface CombinedState {
-//   users: UsersSlice;
-//   liveChatSesssion: LiveChatSessionSlice;
-//   chatHistory: ChatHistorySlice;
-// }
-
-// export type StateSlice<T> = StateCreator<CombinedState, [], [], T>;
-
-// const useLiveChatStore = create<CombinedState>()((...api) => ({
-//     users: createUsersSlice(...api),
-//     liveChatSesssion: createLiveChatSessionSlice(...api),
-//     chatHistory: createChatHistorySlice(...api),
-// }));
-
-// export default useLiveChatStore;
-
-
 import { StateCreator, create } from "zustand";
 import { immer } from "zustand/middleware/immer";
-import createLiveChatSessionSlice, { LiveChatSessionSlice } from "./slices/livechatSessionSlice";
-import createChatHistorySlice, { ChatHistorySlice } from "./slices/chathistorySlice";
+import { createJSONStorage, devtools, persist } from "zustand/middleware";
+import createLiveChatSessionSlice, {
+  LiveChatSessionSlice,
+} from "./slices/livechatSessionSlice";
+import createChatHistorySlice, {
+  ChatHistorySlice,
+} from "./slices/chathistorySlice";
 import createUsersSlice, { UsersSlice } from "./slices/usersSlice";
 
 export interface CombinedState {
@@ -32,12 +15,47 @@ export interface CombinedState {
   chatHistory: ChatHistorySlice;
 }
 
-export type StateSlice<T> = StateCreator<CombinedState, [['zustand/immer', never]], [], T>;
+export type StateSlice<T> = StateCreator<
+  CombinedState,
+  [["zustand/immer", never], ["zustand/devtools", never]],
+  [["zustand/persist", Partial<T>]],
+  T
+>;
 
-const useLiveChatStore = create<CombinedState>()(immer((...api) => ({
-    users: createUsersSlice(...api),
-    liveChatSesssion: createLiveChatSessionSlice(...api),
-    chatHistory: createChatHistorySlice(...api),
-})));
+const useLiveChatStore = create<CombinedState>()(
+  devtools(
+    persist(
+      immer((...api) => ({
+        users: createUsersSlice(...api),
+        liveChatSesssion: createLiveChatSessionSlice(...api),
+        chatHistory: createChatHistorySlice(...api),
+      })),
+      {
+        name: "liveChatStore",
+        storage: createJSONStorage(() => sessionStorage),
+        merge: (persistedState, currentState) => {
+          const typedPersistedState = persistedState as
+            | CombinedState
+            | undefined;
+          return {
+            users: {
+              ...currentState.users,
+              ...(typedPersistedState?.users || {}),
+            },
+            chatHistory: {
+              ...currentState.chatHistory,
+              ...(typedPersistedState?.chatHistory || {}),
+            },
+            liveChatSesssion: {
+              ...currentState.liveChatSesssion,
+              ...(typedPersistedState?.liveChatSesssion || {}),
+            },
+          };
+        },
+      }
+    ),
+    { name: "global-store" }
+  )
+);
 
 export default useLiveChatStore;
--- src/utils/renderRouterWrapper.tsx
+++ src/utils/renderRouterWrapper.tsx
@@ -2,8 +2,9 @@ import { render } from "@testing-library/react";
 import { createMemoryHistory } from "history";
 import { Router } from "react-router";
 
-export const renderRouterWrapper = (Component: React.ReactNode) => {
+export const renderRouterWrapper = (Component: React.ReactNode, currentPath?: string) => {
   const history = createMemoryHistory();
+  history.push(currentPath || '/')
   return {...render(
     <Router location={history.location} navigator={history}>
       {Component}
